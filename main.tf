4179d79246cbc15c7062e4aec61ff0f7d489c2d9
Eugene Chia <eugeneckk@gmail.com>
2025-05-13 10:46:36 +0800

    updated files

--- main.tf
+++ main.tf
@@ -13,18 +13,18 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "tf-rg" {
-  name     = "tf-rg"
+resource "azurerm_resource_group" "tf-test-rg" {
+  name     = "tf-test-rg"
   location = "Southeast Asia"
   tags = {
     environment = "dev"
   }
 }
 
-resource "azurerm_virtual_network" "eugene-lab-vnet" {
-  name                = "eugene-lab-vnet"
-  location            = azurerm_resource_group.tf-rg.location
-  resource_group_name = azurerm_resource_group.tf-rg.name
+resource "azurerm_virtual_network" "tf-test-vnet" {
+  name                = "tf-test-vnet"
+  location            = azurerm_resource_group.tf-test-rg.location
+  resource_group_name = azurerm_resource_group.tf-test-rg.name
   address_space       = ["10.0.0.0/16"]
 
   tags = {
@@ -32,24 +32,24 @@ resource "azurerm_virtual_network" "eugene-lab-vnet" {
   }
 }
 
-resource "azurerm_subnet" "eugene-lab-subnet" {
-  name                 = "eugene-lab-subnet"
-  resource_group_name  = azurerm_resource_group.tf-rg.name
-  virtual_network_name = azurerm_virtual_network.eugene-lab-vnet.name
+resource "azurerm_subnet" "tf-test-subnet" {
+  name                 = "tf-test-subnet"
+  resource_group_name  = azurerm_resource_group.tf-test-rg.name
+  virtual_network_name = azurerm_virtual_network.tf-test-vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "eugene-lab-sg" {
+resource "azurerm_network_security_group" "tf-test-sg" {
   name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.tf-rg.location
-  resource_group_name = azurerm_resource_group.tf-rg.name
+  location            = azurerm_resource_group.tf-test-rg.location
+  resource_group_name = azurerm_resource_group.tf-test-rg.name
 
   tags = {
     environment = "Dev"
   }
 }
 
-resource "azurerm_network_security_rule" "eugene-lab-sg-security-rule" {
+resource "azurerm_network_security_rule" "tf-test-sg-security-rule" {
   name                        = "dev-rule"
   priority                    = 100
   direction                   = "Inbound"
@@ -59,19 +59,19 @@ resource "azurerm_network_security_rule" "eugene-lab-sg-security-rule" {
   destination_port_range      = "*"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.tf-rg.name
-  network_security_group_name = azurerm_network_security_group.eugene-lab-sg.name
+  resource_group_name         = azurerm_resource_group.tf-test-rg.name
+  network_security_group_name = azurerm_network_security_group.tf-test-sg.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "eugene-lab-sga" {
-  subnet_id                 = azurerm_subnet.eugene-lab-subnet.id
-  network_security_group_id = azurerm_network_security_group.eugene-lab-sg.id
+resource "azurerm_subnet_network_security_group_association" "tf-test-sga" {
+  subnet_id                 = azurerm_subnet.tf-test-subnet.id
+  network_security_group_id = azurerm_network_security_group.tf-test-sg.id
 }
 
-resource "azurerm_public_ip" "eugene-lab-publicip" {
+resource "azurerm_public_ip" "tf-test-publicip" {
   name                = "acceptanceTestPublicIp1"
-  resource_group_name = azurerm_resource_group.tf-rg.name
-  location            = azurerm_resource_group.tf-rg.location
+  resource_group_name = azurerm_resource_group.tf-test.name
+  location            = azurerm_resource_group.tf-test.location
   allocation_method   = "Static"
 
   tags = {
@@ -79,16 +79,16 @@ resource "azurerm_public_ip" "eugene-lab-publicip" {
   }
 }
 
-resource "azurerm_network_interface" "eugene-lab-nic" {
-  name                = "eugene-lab-nic"
-  location            = azurerm_resource_group.tf-rg.location
-  resource_group_name = azurerm_resource_group.tf-rg.name
+resource "azurerm_network_interface" "tf-test-nic" {
+  name                = "tf-test-nic"
+  location            = azurerm_resource_group.tf-test-rg.location
+  resource_group_name = azurerm_resource_group.tf-test-rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.eugene-lab-subnet.id
+    subnet_id                     = azurerm_subnet.tf-test-subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.eugene-lab-publicip.id
+    public_ip_address_id          = azurerm_public_ip.tf-test-publicip.id
   }
 
   tags = {
@@ -96,13 +96,13 @@ resource "azurerm_network_interface" "eugene-lab-nic" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "eugene-lab-vm" {
-  name                  = "eugene-vm"
-  resource_group_name   = azurerm_resource_group.tf-rg.name
-  location              = azurerm_resource_group.tf-rg.location
+resource "azurerm_linux_virtual_machine" "tf-test-vm" {
+  name                  = "tf-test-vm"
+  resource_group_name   = azurerm_resource_group.tf-test-rg.name
+  location              = azurerm_resource_group.tf-test-rg.location
   size                  = "Standard_B1s"
   admin_username        = "eugenechia"
-  network_interface_ids = [azurerm_network_interface.eugene-lab-nic.id]
+  network_interface_ids = [azurerm_network_interface.tf-test-nic.id]
 
   custom_data = filebase64("customdata.tpl")
 
@@ -137,7 +137,7 @@ resource "azurerm_linux_virtual_machine" "eugene-lab-vm" {
   }
 }
 
-data "azurerm_public_ip" "eugene-lab-data" {
-  name                = azurerm_public_ip.eugene-lab-publicip.name
-  resource_group_name = azurerm_resource_group.tf-rg.name
+data "azurerm_public_ip" "tf-test-data" {
+  name                = azurerm_public_ip.tf-test-publicip.name
+  resource_group_name = azurerm_resource_group.tf-test-rg.name
 }
